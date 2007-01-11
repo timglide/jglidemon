6fa98d7643ac898e03231ebef2470f706c71394b
tim <tim@66374db6-e547-0410-9f89-9d3a8c05f541>
2007-01-11 21:49:40 +0000

    Added TTS, rewrote Sound subsystem

--- jgm/cfg.java
+++ jgm/cfg.java
@@ -2,6 +2,12 @@ package jgm;
 
 import jgm.util.*;
 
+/**
+ * Contains all global configuration values and handles
+ * reading and writing to the ini file.
+ * @author Tim
+ * @since 0.1
+ */
 public class cfg extends Thread {
 	private boolean set = false;
 	private static QuickIni ini = new QuickIni("JGlideMon.ini");
@@ -41,15 +47,29 @@ public class cfg extends Thread {
 		screenshot.autoUpdate = ini.getBooleanProperty("screenshot", "autoupdate", true);
 		screenshot.updateInterval = ini.getIntegerProperty("screenshot", "updateinterval", 5000);
 		screenshot.scale = ini.getIntegerProperty("screenshot", "scale", 100);
+		screenshot.quality = ini.getIntegerProperty("screenshot", "quality", 50);
 		
 		if (screenshot.scale > 100) screenshot.scale = 100; else
 		if (screenshot.scale < 10)  screenshot.scale = 10;
+		
+		if (screenshot.quality > 100) screenshot.quality = 100; else
+		if (screenshot.quality < 10)  screenshot.quality = 10;
 		
 		window.x = ini.getIntegerProperty("window", "x", 50);
 		window.y = ini.getIntegerProperty("window", "y", 50);
 		window.maximized = ini.getBooleanProperty("window", "maximized", true);
 		window.width = ini.getIntegerProperty("window", "width", 1000);
 		window.height = ini.getIntegerProperty("window", "height", 700);
+		
+		sound.enabled = ini.getBooleanProperty("sound", "enabled", true);
+		sound.whisper = ini.getBooleanProperty("sound", "whisper", true);
+		sound.say = ini.getBooleanProperty("sound", "say", true);
+		sound.gm = ini.getBooleanProperty("sound", "gm", true);
+		sound.tts.enabled = ini.getBooleanProperty("sound.tts", "enabled", true);
+		sound.tts.whisper = ini.getBooleanProperty("sound.tts", "whisper", true);
+		sound.tts.say = ini.getBooleanProperty("sound.tts", "say", false);
+		sound.tts.gm = ini.getBooleanProperty("sound.tts", "gm", true);
+		sound.tts.status = ini.getBooleanProperty("sound.tts", "status", true);
 		
 		notifyAll();
 	}
@@ -67,12 +87,23 @@ public class cfg extends Thread {
 				ini.setBooleanProperty("screenshot", "autoupdate", screenshot.autoUpdate);
 				ini.setIntegerProperty("screenshot", "updateinterval", screenshot.updateInterval);
 				ini.setIntegerProperty("screenshot", "scale", screenshot.scale);
+				ini.setIntegerProperty("screenshot", "quality", screenshot.quality);
 		
 				ini.setIntegerProperty("window", "x", window.x);
 				ini.setIntegerProperty("window", "y", window.y);
 				ini.setBooleanProperty("window", "maximized", window.maximized);
 				ini.setIntegerProperty("window", "width", window.width);
 				ini.setIntegerProperty("window", "height", window.height);				
+				
+				ini.setBooleanProperty("sound", "enabled", sound.enabled);
+				ini.setBooleanProperty("sound", "whisper", sound.whisper);
+				ini.setBooleanProperty("sound", "say", sound.say);
+				ini.setBooleanProperty("sound", "gm", sound.gm);
+				ini.setBooleanProperty("sound.tts", "enabled", sound.tts.enabled);
+				ini.setBooleanProperty("sound.tts", "whisper", sound.tts.whisper);
+				ini.setBooleanProperty("sound.tts", "say", sound.tts.say);
+				ini.setBooleanProperty("sound.tts", "gm", sound.tts.gm);
+				ini.setBooleanProperty("sound.tts", "status", sound.tts.status);
 				
 				ini.updateFile();
 			}
@@ -95,6 +126,7 @@ public class cfg extends Thread {
 		public static boolean autoUpdate = true;
 		public static int updateInterval = 5000;
 		public static int scale = 100;
+		public static int quality = 100;
 	}
 	
 	public static class window {
@@ -103,5 +135,20 @@ public class cfg extends Thread {
 		public static boolean maximized = true;
 		public static int width = 1000;
 		public static int height = 700;
+	}
+	
+	public static class sound {
+		public static boolean enabled = true;
+		public static boolean whisper = true;
+		public static boolean say = true;
+		public static boolean gm = true;
+		
+		public static class tts {
+			public static boolean enabled = true;
+			public static boolean whisper = true;
+			public static boolean say = true;
+			public static boolean gm = true;
+			public static boolean status = true;
+		}
 	}
 }
