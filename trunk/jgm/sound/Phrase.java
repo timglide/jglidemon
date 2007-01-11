e5fc75dc8a2282b9863c0f8f70399663e3870e6a
tim <tim@66374db6-e547-0410-9f89-9d3a8c05f541>
2007-01-11 21:47:55 +0000

    Added TTS, rewrote Sound subsystem

---
+++ trunk/jgm/sound/Phrase.java
@@ -0,0 +1,40 @@
+package jgm.sound;
+
+import jgm.cfg;
+import jgm.util.Speech;
+
+/**
+ * Represents a sound that will be generated with TTS.
+ * @author Tim
+ * @since 0.3
+ */
+public class Phrase extends Audible {
+	private String text;
+	
+	/**
+	 * Construct a phrase.
+	 * @param t The type of sound
+	 * @param s The text to say
+	 */
+	public Phrase(Type t, String s) {
+		super(t);
+		text = s;
+	}
+	
+	protected void createSound() {		
+		Speech.speak(text);
+	}
+	
+	public boolean isAudible() {
+		if (!cfg.sound.tts.enabled) return false;
+		
+		switch (type) {
+			case WHISPER: return cfg.sound.tts.whisper;
+			case SAY:     return cfg.sound.tts.say;
+			case GM:      return cfg.sound.tts.gm;
+			case STATUS:  return cfg.sound.tts.status;
+		}
+		
+		return false;
+	}
+}
--- trunk/jgm/util/Sound.java
+++ trunk/jgm/util/Sound.java
@@ -1,29 +1,75 @@
-package jgm;
+package jgm.util;
 
 import java.applet.Applet;
-import java.applet.AudioClip;;
+import java.applet.AudioClip;
 
+/**
+ * Handles the available sound files that can be played.
+ * @author Tim
+ */
 public class Sound {
-	public static final int WHISPER = 0;
-	public static final int GM_WHISPER = 1;
-	
-	private static AudioClip[] clips = new AudioClip[2];
+	/**
+	 * An available sound file.
+	 * @author Tim
+	 */
+	public enum File {
+		WHISPER    ("Whisper.wav",   1309),
+		GM_WHISPER ("GMWhisper.wav", 3400);
+		
+		private final AudioClip clip;
+		private final int length;
+		
+		/**
+		 * Construct a sound file.
+		 * @param name The filename of the sound file
+		 * @param len The duration of the sound in ms
+		 */
+		private File(String name, int len) {
+			length = len;
+			
+			clip = Applet.newAudioClip(
+				jgm.JGlideMon.class.getResource("resources/sounds/" + name)
+			);
+		}
+		
+		/**
+		 * Play the sound.
+		 */
+		public void play() {
+			play(waitForCompletion);
+		}
+		
+		/**
+		 * Play the sound
+		 * @param wait Whether to wait for the sounnd to finish playing before returning
+		 */
+		public void play(boolean wait) {
+			clip.play();
+			
+			if (wait) {
+				try {
+					Thread.sleep(length);
+				} catch (InterruptedException e) {}
+			}
+		}
+	};
+		
+	public static boolean waitForCompletion = true;
 	
 	private Sound() {}
 	
-	public static void init() {
-		clips[WHISPER] = Applet.newAudioClip(
-			JGlideMon.class.getResource("resources/sounds/Whisper.wav")
-		);
+	public static void init() {}
+	
+	public final static void play(File sound) {		
+		long t1, t2;
 		
-		clips[GM_WHISPER] = Applet.newAudioClip(
-			JGlideMon.class.getResource("resources/sounds/GMWhisper.wav")
-		);
-	}
-		
-	public final static void playSound(int sound) {
 		try {
-			clips[sound].play();
+			t1 = System.currentTimeMillis();
+			sound.play(waitForCompletion);
+			
+			t2 = System.currentTimeMillis();
+			
+			System.out.println("Time to play sound " + sound + ": " + (t2 - t1) + "ms");
 		} catch (Exception e) {
 			System.err.println("Error playing sound #" + sound + ": " + e.getMessage());
 		}
