c2cb5ee1bd340b6652c91515d6f0b6a19b2b9638
tim <tim@66374db6-e547-0410-9f89-9d3a8c05f541>
2007-01-12 22:03:29 +0000

    Updated documentation

--- jgm/gui/tabs/Tab.java
+++ jgm/gui/tabs/Tab.java
@@ -5,14 +5,28 @@ import jgm.gui.updaters.StatusUpdater;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Abstract class representing one of the main tabs.
+ * @author Tim
+ * @since 0.1
+ */
 public abstract class Tab extends jgm.gui.panes.Pane {
 	public String name;
 
+	/**
+	 * Create a Tab with the specified name.
+	 * @param s The name of the Tab
+	 */
 	public Tab(String s) {
 		super();
 		name = s;
 	}
 
+	/**
+	 * Create a Tab with the specified name and LayoutManager.
+	 * @param lm The LayoutManager to use
+	 * @param s The name of the Tab
+	 */
 	public Tab(LayoutManager lm, String s) {
 		super(lm);
 		name = s;
@@ -22,14 +36,26 @@ public abstract class Tab extends jgm.gui.panes.Pane {
 		return true;
 	}
 	
+	/**
+	 * Returns the index of this Tab.
+	 * @return The index of this Tab
+	 */
 	public int getIndex() {
 		return ((JTabbedPane) this.getParent()).indexOfTab(name);
 	}
 	
+	/**
+	 * Determines if this Tab is currently selected.
+	 * @return Whether this Tab is currently selected
+	 */
 	public boolean isCurrentTab() {
 		return ((JTabbedPane) this.getParent()).getSelectedIndex()
 				== getIndex();
 	}
 	
+	/**
+	 * It is optional for a Tab to implement this method
+	 * for when the StatusUpdater has updated.
+	 */
 	public void update(StatusUpdater s) {}
 }
