0f8943e617148dc1b190e0d766e4bc42cad61d54
tim <tim@66374db6-e547-0410-9f89-9d3a8c05f541>
2007-06-25 00:45:21 +0000

    Final changes for 0.10

--- branches/0.x/jgm/JGlideMon.java
+++ branches/0.x/jgm/JGlideMon.java
@@ -33,13 +33,13 @@ import jgm.util.*;
  */
 public class JGlideMon {
 	public static final String app = "JGlideMon";
-	public static final String version = "0.10 beta";
+	public static final String version = "0.10";
 	public static final String _revision = "$Revision$";
 	public static final String revision = _revision.substring(1, _revision.length() - 1);
 	public static final String _date = "$Date$";
 	public static final String date = _date.substring(1, _date.length() - 1);
 	
-	public static boolean debug = true;
+	public static boolean debug = false;
 	
 	public static JGlideMon instance;
 	
