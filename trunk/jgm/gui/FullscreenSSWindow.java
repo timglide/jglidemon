10032a425a8d715d1c7879dc4b87e3a9dccfc8a8
timglide <timglide@66374db6-e547-0410-9f89-9d3a8c05f541>
2008-03-08 07:58:35 +0000

    Improved fullscreen screenshot mode.

--- trunk/jgm/gui/FullscreenSSWindow.java
+++ trunk/jgm/gui/FullscreenSSWindow.java
@@ -1,7 +1,5 @@
 package jgm.gui;
 
-import jgm.gui.dialogs.Dialog;
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -10,27 +8,50 @@ public class FullscreenSSWindow extends FullscreenWindow {
 	final GUI gui;
 	public JPanel ssPanel = null;
 	
-	public FullscreenSSWindow(GUI gui) {
+	public FullscreenSSWindow(final GUI gui) {
 		super(Color.BLACK);
+		setTitle("Screenshot - " + jgm.JGlideMon.app);
 		this.gui = gui;
 
 		setLayout(new BorderLayout());
 		
-		JButton close = new JButton("Close");
-		close.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				FullscreenSSWindow.this.gui.menu.normalSS.doClick();
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				gui.menu.normalSS.doClick();
 			}
 		});
 		
-		JButton die = new JButton("Die");
-		die.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
+		// so pressing F11 will close the window
+		getRootPane()
+			.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+			.put(KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0), "closeWindow");
+		getRootPane()
+			.getActionMap()
+			.put("closeWindow", new AbstractAction() {
+				public void actionPerformed(ActionEvent e) {
+					gui.menu.normalSS.doClick();
+				}
+			});
 		
-		add(Dialog.makeNiceButtons(close, die), BorderLayout.SOUTH);
+		
+		// only way out is F11/Alt-F4
+//		JButton close = new JButton("Close");
+//		close.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				FullscreenSSWindow.this.gui.menu.normalSS.doClick();
+//			}
+//		});
+//		
+//		JButton die = new JButton("Die");
+//		die.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				System.exit(0);
+//			}
+//		});
+//		
+//		add(Dialog.makeNiceButtons(close, die), BorderLayout.SOUTH);
 	}
 	
 	int lastGuiState = 0;
