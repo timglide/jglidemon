a86d177923a0d826617b9e8a40773e74689f1a89
timglide <timglide@66374db6-e547-0410-9f89-9d3a8c05f541>
2012-02-20 20:57:58 +0000

    Changed regex to detect experience from killing mob to be better, converted http server to use jquery/json instead of prototype/xml, corrected utf-8 encoding of web server.

--- src/jgm/httpd/AjaxHandler.java
+++ src/jgm/httpd/AjaxHandler.java
@@ -20,24 +20,16 @@
  */
 package jgm.httpd;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
-import javax.xml.transform.dom.*; 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.io.*;
 import java.util.Properties;
 
 import jgm.JGlideMon;
 
 public class AjaxHandler extends Handler {
-	public static final String DEF_ERROR_XML = 
-		"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\nn" +
-		"<response>\n" + 
-		"<status>failure</status>\n" +
-		"<message/>\n" +
-		"</response>";
+	public static final String DEF_ERROR_JSON =
+		"{status: \"failure\", message: \"\"}";
 	
 	public AjaxHandler(HTTPD httpd) {
 		super(httpd);
@@ -46,12 +38,12 @@ public class AjaxHandler extends Handler {
 	@Override
 	public Response handle(String uri, String method, Properties headers, Properties params) {
 		String out = createXML(uri, method, headers, params);
-		if (out == null) out = DEF_ERROR_XML;
+		if (out == null) out = DEF_ERROR_JSON;
 		
 		Response ret = null;
 		
 		ret = new Response(HTTPD.HTTP_OK, HTTPD.MIME_HTML, out);
-		ret.addHeader("Content-type", "text/xml");
+		ret.addHeader("Content-type", "application/json; charset=utf-8");
 		
 		// add no-cache headers
 		ret.addHeader("Cache-Control", "no-cache, must-revalidate");
@@ -61,52 +53,15 @@ public class AjaxHandler extends Handler {
 	}
 
 	private String createXML(String uri, String method, Properties headers, Properties params) {
-		Document xml = null;
-		DocumentBuilderFactory factory = 
-			DocumentBuilderFactory.newInstance();
-		
+		JSONObject root = new JSONObject();
 		final jgm.ServerManager sm = httpd.sm;
 		
 		try {
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			xml = builder.newDocument();  // Create from whole cloth
-	
-			Element root = CE(xml, "response"); 
-			AC(xml, root);
-			
 			// status
-			Element tmp = CE(xml, "status");
-			Text status = CTN(xml, "success");
-			AC(tmp, status);
-			AC(root, tmp);
+			root.put("status", "success");
 			
 			// for an error message
-			tmp = CE(xml, "message");
-			Text message = CTN(xml, "");
-			AC(tmp, message);
-			AC(root, tmp);
-			
-			
-/*			Element request = CE(xml, "request");
-			AC(root, request);
-			
-			// url
-			tmp = CE(xml, "url");
-			AC(tmp, CTN(xml, uri));
-			AC(request, tmp);
-	
-			// headers
-			tmp = CE(xml, "headers");
-			
-			for (Object key : headers.keySet()) {
-				Element tmp2 = CE(xml, "header");
-				tmp2.setAttribute("name", key.toString());
-				tmp2.appendChild(CTN(xml, headers.get(key).toString()));
-				AC(tmp, tmp2);
-			}
-			
-			AC(request, tmp);
-			*/
+			root.put("message", "");
 			
 			if (uri.equals("command")) {
 				final String cmd = params.getProperty("command");
@@ -141,56 +96,55 @@ public class AjaxHandler extends Handler {
 								}
 							};
 						} else {
-							status.setTextContent("error");
-							message.setTextContent("No keys provided for sending");
+							root.put("status", "error");
+							root.put("message", "No keys provided for sending");
 						}
 					} else {
-						status.setTextContent("error");
-						message.setTextContent("Invalid command: " + cmd);
+						root.put("status", "error");
+						root.put("message", "Invalid command: " + cmd);
 					}
 					
 					if (t != null) {
 						t.start();
 					}
 				} else {
-					status.setTextContent("error");
-					message.setTextContent("No command given");
+					root.put("status", "error");
+					root.put("message", "No command given");
 				}
 			} else if (uri.equals("status")) {
 				// jgm info
-				Element jgm_ = CE(xml, "app");
-				_(xml, jgm_, "name", JGlideMon.app);
-				_(xml, jgm_, "version", JGlideMon.version);
+				JSONObject jgm_ = CE(root, "app");
+				jgm_.put("name", JGlideMon.app);
+				jgm_.put("version", JGlideMon.version);
 				
 				boolean connected = httpd.sm.connector.isConnected();
-				_(xml, jgm_, "connected", Boolean.toString(connected));
+				jgm_.put("connected", Boolean.toString(connected));
 				
-				_(xml, jgm_, "update-interval", jgm.Config.getInstance().getInt("web.updateinterval"));
+				jgm_.put("update-interval", jgm.Config.getInstance().getInt("web.updateinterval"));
 				
-				AC(root, jgm_);
 				
 				
 				// glider info
-				Element glider = CE(xml, "glider");
+				JSONObject glider = CE(root, "glider");
 				
 				if (connected && sm.status != null) {
 					jgm.glider.Status s = sm.status.s;
 					
-					_(xml, glider, "version", s.version);
-					_(xml, glider, "attached", Boolean.toString(s.attached));
+					glider.put("version", s.version);
+					glider.put("attached", Boolean.toString(s.attached));
 					
 					if (s.attached) {
-						_(xml, glider, "lcclass", s.clazz.toString().toLowerCase().replace(' ', '_'));
-						_(xml, glider, "class", s.clazz.toString());
-						_(xml, glider, "name", s.name);
-						_(xml, glider, "level", s.level);
-						_(xml, glider, "health", (int) s.health);
-						_(xml, glider, "mana", (int) s.mana);
-						_(xml, glider, "mana-name", s.manaName.toLowerCase());
-						_(xml, glider, "xp", s.experience);
-						_(xml, glider, "next-xp", s.nextExperience);
-						_(xml, glider, "xp-percent", (int) (100.0 * ((double) s.experience) / s.nextExperience));
-						_(xml, glider, "xp-per-hour", s.xpPerHour);
+						glider.put("lcclass", s.clazz.toString().toLowerCase().replace(' ', '_'));
+						glider.put("class", s.clazz.toString());
+						glider.put("name", s.name);
+						glider.put("level", s.level);
+						glider.put("health", (int) s.health);
+						glider.put("mana", (int) s.mana);
+						glider.put("mana-name", s.manaName.toLowerCase());
+						glider.put("xp", s.experience);
+						glider.put("next-xp", s.nextExperience);
+						glider.put("xp-percent", (int) (100.0 * ((double) s.experience) / s.nextExperience));
+						glider.put("xp-per-hour", s.xpPerHour);
 						
 						if (s.xpPerHour > 0) {
 							int seconds = 0, minutes = 0, hours = 0;
@@ -202,109 +156,54 @@ public class AjaxHandler extends Handler {
 							d = 60 * (d - minutes);
 							seconds = (int) d;
 							
-							_(xml, glider, "ttl", String.format("%d:%02d:%02d", hours, minutes, seconds));
+							glider.put("ttl", String.format("%d:%02d:%02d", hours, minutes, seconds));
 						} else {
-							_(xml, glider, "ttl", "Unknown");
+							glider.put("ttl", "Unknown");
 						}
 						
-						_(xml, glider, "mode", s.mode);
-						_(xml, glider, "full-profile", s.profile);
+						glider.put("mode", s.mode);
+						glider.put("full-profile", s.profile);
 						
 						String[] parts = s.profile.split("\\\\");
 						String str = parts.length > 0 ? parts[parts.length - 1] : "";
-						_(xml, glider, "profile", str);
+						glider.put("profile", str);
 						
-						_(xml, glider, "kills", s.kills);
-						_(xml, glider, "loots", s.loots);
-						_(xml, glider, "deaths", s.deaths);
+						glider.put("kills", s.kills);
+						glider.put("loots", s.loots);
+						glider.put("deaths", s.deaths);
 						
-						_(xml, glider, "location", s.location);
-						_(xml, glider, "heading", s.heading);
+						glider.put("location", s.location);
+						glider.put("heading", s.heading);
 						
-						_(xml, glider, "target-name", s.targetName);
-						_(xml, glider, "target-level", s.targetLevel);
-						_(xml, glider, "target-health", (int) s.targetHealth);
+						glider.put("target-name", s.targetName);
+						glider.put("target-level", s.targetLevel);
+						glider.put("target-health", (int) s.targetHealth);
 					}
 				}
-				
-				AC(root, glider);
 			} else {
-				status.setTextContent("error");
-				message.setTextContent("Invalid uri: " + uri);
+				root.put("status", "error");
+				root.put("message", "Invalid uri: " + uri);
 			}
 			
-			
-			// prepare for serialization
-			xml.setXmlStandalone(true);
-			xml.getDocumentElement().normalize();
-			
-//			Serialisation through Tranform.
-			StringWriter out = new StringWriter();
-			DOMSource domSource = new DOMSource(xml);
-			StreamResult streamResult = new StreamResult(out);
-			TransformerFactory tf = TransformerFactory.newInstance();
-			Transformer serializer = tf.newTransformer();
-			serializer.setOutputProperty(OutputKeys.ENCODING,"UTF-8");
-//			serializer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM,"users.dtd");
-			serializer.setOutputProperty(OutputKeys.INDENT,"yes");
-			serializer.transform(domSource, streamResult); 
-			
-			return out.toString();
+			return root.toString();
 		} catch (Throwable pce) {
 			// this had better not happen
 			
 			// Parser with specified options can't be built
 			pce.printStackTrace();
-			System.exit(1);
 		}
 		
 		return null;
 	}
-	
-	// cuz i don't want to have to type this out each fucking time
-	
+		
 	/**
-	 * One liner to make a new tag with a text node and append
-	 * it to parent when you won't need to modify it later.
+	 * Creates a new json object and appends it to parent with the given
+	 * key.
+	 * @throws JSONException 
 	 */
-	private void _(Document xml, Element parent, String tag, Object contents) {
-		Element tmp = CE(xml, tag);
-		AC(tmp, CTN(xml, contents.toString()));
-		AC(parent, tmp);
-	}
-	
-	/**
-	 * createElement shortcut
-	 */
-	private Element CE(Document xml, String name) {
-		return xml.createElement(name);
-	}
-	
-	/**
-	 * createTextNode shortcut
-	 * @param xml
-	 * @param contents
-	 * @return
-	 */
-	private Text CTN(Document xml, String contents) {
-		return xml.createTextNode(contents);
-	}
-	
-	/**
-	 * appendChild shortcut
-	 * @param parent
-	 * @param child
-	 */
-	private void AC(Document parent, Element child) {
-		parent.appendChild(child);
-	}
-	
-	/**
-	 * appendChild shortcut
-	 * @param parent
-	 * @param child
-	 */
-	private void AC(Element parent, Node child) {
-		parent.appendChild(child);
+	private JSONObject CE(JSONObject parent, String name) throws JSONException {
+		JSONObject obj = new JSONObject();
+		parent.put(name, obj);
+		return obj;
 	}
 }
